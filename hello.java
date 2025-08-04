cecad327c650a66f012e165c1df54696806c377f
mavenhay <maven.hay@gmail.com>
2025-08-04 13:16:15 +0700

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 public class hello {
     public static void main(String[] args){
-        System.out.println("Maven");
+        System.out.println("""
+                Nama: Maven Helios Agathon Yesstian
+                Sekolah asal: SMPN 1 Pacet Mojokerto
+                Nama ayah: Wahyu Setianto
+                Nama ibu: Aita Yessi Silia
+                Cita-cita: Menjadi programmer yang menciptakan inovasi baru dan penemuan
+                Hobi: Coding website atau game, bermain video game, jalan-jalan
+                """);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input Y number: ");
+        int y = scanner.nextInt();
         int x = 0;
-        while (x<=10) {
+        double z = 0;
+        while (x<=12*y) {
+            System.out.print(x);
             if (x%2 == 0) {
-                System.out.println("Genap");
+                System.out.println(" = Genap");
             } else {
-                System.out.println("Ganjil");
+                System.out.println(" = Ganjil");
             }
-            x+=1;
+            z = Math.round(Math.random());
+            x+=y;
+            x+=z;
         }
     }
 }
