07b84d611ba4abbb782c19992efcaa93028ccdd2
mavenhay <maven.hay@gmail.com>
2025-08-04 14:01:15 +0700

    Add files via upload

---
+++ mavenVariabel.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+
+public class mavenVariabel {
+    public static void main(String[] args) {
+        String[] nama = {"Sakti Mahayana Zaman", "Muhammad Jericho Husein", "Muhammad Ilham Daniswara", "Iqbal Ibrahim Widycahyono", "Intazar Lintang Pramanta"};
+        int[] umur = {15,15,15,15,16};
+        String[] cita = {"Sukses", "Miliarder", "Software engineer", "Pembisnis", "IT Developer"};
+
+        int x = 0;
+        while(x<=4) {
+            System.out.println("Teman "+ (x+1));
+            System.out.println("Nama: " + nama[x]);
+            System.out.println("Umur: " + umur[x]);
+            System.out.println("Cita-cita: " + cita[x] + "\n");
+            x+=1;
+        }
+    }
+}
